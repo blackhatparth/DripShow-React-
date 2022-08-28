1de5c9a68f39ed29d799ed04d76f2c107d8016a2
Parth Sarthi Shukla <parthshukla12345@gmail.com>
2022-08-28 18:33:16 +0600

    Web changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Home from './components/home';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
