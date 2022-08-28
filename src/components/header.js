c69102663839b567e49fb97627914de3a0f2d9f7
Parth Sarthi Shukla <parthshukla12345@gmail.com>
2022-08-28 17:47:55 +0600

    header updated

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+function Header(){
+    return (
+    <header>
+        <div className='navbar'>
+        <a href="#home">Home</a>
+        <div className="dropdown">
+        <button className="dropbtn">Products
+            <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="dropdown-content">
+            <a href="#">DripShow Store Finder</a>
+            <a href="#">DripShow Delivery</a>
+            <a href="#">Digital Marketing</a>
+        </div>
+        </div>
+        <div className="dropdown">
+        <button className="dropbtn">Company
+            <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="dropdown-content">
+            <a href="#">Careers</a>
+            <a href="#">About us</a>
+            <a href="#">Contact us</a>
+        </div>
+        </div>
+            <a href="#home">Pricing</a>
+        <a href="login.html">Log in</a>
+        <button type="button"> <a href="#">Start free trial</a></button>
+        </div>
+        
+    </header>
+        );
+}
+
+export default Header;
