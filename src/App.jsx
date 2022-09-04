f572f53ed3cb492d73bad35ca0bca5943f8e9f26
Parth Sarthi Shukla <parthshukla12345@gmail.com>
2022-09-04 12:28:59 +0600

    Nav updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Home from './Pages/Home/Home'
+import About from './Pages/About Us/Aboutus'
+import Careers from './Pages/Careers/Careers'
+import Contact from './Pages/Contact Us/Contact'
+import Comingsoon from './Pages/Coming Soon/Comingsoon'
+import Delivery from './Pages/DripShow Delivery/delivery'
+import Storelocator from './Pages/DripShow Store Locator/storelocator'
+import Membership from './Pages/Membership/Membership'
+import NotFound from './Pages/Not found/Notfound'
+import Navbar from './Components/Navbar'
+import Aboutus from './Pages/About Us/Aboutus'
 
 const App = () => {
   return (
-    <div>React Website</div>
+    <BrowserRouter>
+      <Navbar/>
+      <Aboutus/>
+      
+      <Careers/>
+      <Contact/>
+      <Comingsoon/>
+      <Delivery/>
+      <Storelocator/>
+      <Membership/>
+      <NotFound/>
+    </BrowserRouter>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+import {SiOpenaigym} from 'react-icons/si'
+
+
+
+export const links = [
+    {
+        name: "Home",
+        path: '/'
+    },
+    {
+        name: "Membership",
+        path: '/Membership'
+    },
+    {
+        name: "Drip Show Delivery",
+        path: '/delivery'
+    },
+    {
+        name: "Drip Show Store Locator",
+        path: '/storelocator'
+    },
+    {
+        name: "About Us",
+        path: '/Aboutus'
+    },
+    {
+        name: "Careers",
+        path: '/Careers'
+    },
+    {
+        name: "Contact Us",
+        path: '/Contact'
+    }
+]
+
+
+
+
