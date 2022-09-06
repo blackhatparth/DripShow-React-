4ea68238d4a2db92b7838ac58c6e0cdca4dca0e8
Parth Sarthi Shukla <parthshukla12345@gmail.com>
2022-09-07 05:37:44 +0600

    Services added

--- src/Components/Values.jsx
+++ src/Components/Values.jsx
@@ -1,10 +1,39 @@
+import Image from '../Images/values.png'
+import SectionHead from './SectionHead'
+import {GiCutDiamond} from 'react-icons/gi'
+import {values} from '../data'
+import Card from '../UI/Card'
 
 
 const Values = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='values'>
+        <div className="container values__container">
+            <div className="values__left">
+                <div className="values__image">
+                    <img src={Image} alt="" />
+                </div>
+            </div>
+            <div className="values__right">
+                <SectionHead icon={<GiCutDiamond/>} title="Services Offering :"/>
+                <p>
+                    Increase revenue of shop
+                </p>
+                <div className="values__wrapper">
+                    {
+                        values.map(({id, icon, title, desc}) => {
+                            return <Card className="values__value">
+                                <span>{icon}</span>
+                                <h4>{title}</h4>
+                                <small>{desc}</small>
+
+                            </Card>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
